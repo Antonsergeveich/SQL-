b3f5196e29c89105fad98b64a1854676f5219af2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-11-18 09:49:47 +0300

    Add project files.

---
+++ leap year/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+//Високосный если:
+//1)Делится на 4 без остатка
+//2)Делится на 4 на 100 на 400 без остатка
+//Не високосный если: делится на 4 и на 100 без остатка
+//2016, 2000 - yes
+//1900, 2017 - no
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int year;
+	cout << "Введите год: ", cin >> year;
+	if (year % 4 == 0 && year % 100 == 0 && year && 400 == 0)
+	{
+		cout << year << "\tгод високостный" << endl;
+	}
+	else if (year % 4 == 0)
+	{
+		cout << year << "\tгод високостный" << endl;
+	}
+	else if (year % 4 == 0 && year % 100 == 0)
+	{
+		cout << year << "\tгод не високостный" << endl;
+	}
+	else
+	{
+		cout << year << "\tгод не високостный" << endl;
+	}
+}
